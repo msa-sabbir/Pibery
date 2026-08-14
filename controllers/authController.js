d27f39c6d01828b14f8089683515ba2d73339217
Pibery <info.pibery@gmail.com>
2026-08-15 02:30:07 +0600

    Update authController.js

---
+++ controllers/authController.js
@@ -0,0 +1,8 @@
+// Authentication Controller Placeholder
+exports.register = (req, res) => {
+    res.json({ message: 'User registered successfully (Pibery API)' });
+};
+
+exports.login = (req, res) => {
+    res.json({ message: 'User logged in successfully (Pibery API)' });
+};
