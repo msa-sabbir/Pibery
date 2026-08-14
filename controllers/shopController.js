680354a6879d69e6faddbea75e30455f0a4b2260
Pibery <info.pibery@gmail.com>
2026-08-15 02:30:47 +0600

    Update shopController.js

---
+++ controllers/shopController.js
@@ -0,0 +1,8 @@
+// Shop Management Controller Placeholder
+exports.createShop = (req, res) => {
+    res.json({ message: 'New e-commerce shop created successfully!' });
+};
+
+exports.getShopDetails = (req, res) => {
+    res.json({ message: 'Fetching shop data and template view...' });
+};
