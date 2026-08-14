2544de4137ecebaa46a8a94758e96b88ed668606
Pibery <info.pibery@gmail.com>
2026-08-15 02:31:38 +0600

    Update productController.js

---
+++ controllers/productController.js
@@ -0,0 +1,8 @@
+// Product Controller Placeholder
+exports.addProduct = (req, res) => {
+    res.json({ message: 'Product added to shop successfully!' });
+};
+
+exports.getProducts = (req, res) => {
+    res.json({ products: [] });
+};
