deb213677a014759bd90e8bc80067b2d23a23905
Pibery <info.pibery@gmail.com>
2026-08-15 18:48:03 +0600

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { register, login } = require('../controllers/authController');
+const { register, login, logout, getMe } = require('../controllers/authController');
+const { protect } = require('../middleware/auth');
 
 router.post('/register', register);
 router.post('/login', login);
+router.get('/logout', logout);
+router.get('/me', protect, getMe);
 
 module.exports = router;
