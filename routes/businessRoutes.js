dcae64f3768caebaf1067491ccbc03cb2b4d6156
Pibery <info.pibery@gmail.com>
2026-08-15 10:08:33 +0600

    Create routes/businessRoutes.js

---
+++ routes/businessRoutes.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const { createOrder, updateOrderStatus, getReceipt } = require('../controllers/businessController');
+
+router.post('/order/create', createOrder);
+router.put('/order/status', updateOrderStatus);
+router.get('/order/receipt/:orderId', getReceipt);
+
+module.exports = router;
