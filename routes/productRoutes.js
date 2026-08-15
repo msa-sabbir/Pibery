03b1f62e8647f28fca5c4386c8a1c3fab5006d35
Pibery <info.pibery@gmail.com>
2026-08-15 17:05:21 +0600

    Update productRoutes.js

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,8 +1,24 @@
 const express = require('express');
 const router = express.Router();
-const { addProduct, getProducts } = require('../controllers/productController');
+const {
+  createProduct,
+  getShopProducts,
+  getStorefrontProducts,
+  getProduct,
+  updateProduct,
+  deleteProduct,
+  updateStock,
+} = require('../controllers/productController');
+const { protect } = require('../middleware/auth');
 
-router.post('/add', addProduct);
-router.get('/:shopId', getProducts);
+router.get('/storefront/:shopId', getStorefrontProducts); // পাবলিক
+router.get('/:id', getProduct); // পাবলিক
+
+router.use(protect);
+router.post('/', createProduct);
+router.get('/shop/:shopId', getShopProducts);
+router.put('/:id', updateProduct);
+router.delete('/:id', deleteProduct);
+router.patch('/:id/stock', updateStock);
 
 module.exports = router;
