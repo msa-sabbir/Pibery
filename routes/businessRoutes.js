d94beb60e8371465232374ed5db8e9de355d1ff6
Pibery <info.pibery@gmail.com>
2026-08-15 18:50:54 +0600

    Update businessRoutes.js

--- routes/businessRoutes.js
+++ routes/businessRoutes.js
@@ -1,10 +1,29 @@
 const express = require('express');
 const router = express.Router();
-const { createOrder, updateOrderStatus, getReceipt } = require('../controllers/businessController');
+const {
+  getDashboard,
+  getOrders,
+  updateOrderStatus,
+  getCustomers,
+  getCustomerProfile,
+  createCoupon,
+  getCoupons,
+  deleteCoupon,
+} = require('../controllers/businessController');
+const { protect } = require('../middleware/auth');
 
-router.post('/order/create', createOrder);
-router.put('/order/status', updateOrderStatus);
-router.get('/order/receipt/:orderId', getReceipt);
+router.use(protect);
+
+router.get('/:shopId/dashboard', getDashboard);
+
+router.get('/:shopId/orders', getOrders);
+router.patch('/orders/:orderId/status', updateOrderStatus);
+
+router.get('/:shopId/customers', getCustomers);
+router.get('/customers/:customerId', getCustomerProfile);
+
+router.post('/:shopId/marketing', createCoupon);
+router.get('/:shopId/marketing', getCoupons);
+router.delete('/marketing/:id', deleteCoupon);
 
 module.exports = router;
- 
